b8d3ecce8fe010581287d7f6e835a2dd0c0d17b6
David Gualberto <gualbertodavid@gmail.com>
2022-11-07 11:38:33 +0100

    esercizio w7 d1

---
+++ m2/w7/d1/assets/js/script.js
@@ -0,0 +1,61 @@
+btn = document.getElementById('button');
+
+class Utente {
+    constructor(_nome, _cognome, _dataNascita, _anni) {
+        this.nome = _nome;
+        this.cognome = _cognome;
+        this.dataNascita = _dataNascita;
+        this.anni = function() {
+            const oggi = new Date();
+            const annoUtente = new Date(this.dataNascita);
+            var eta = (oggi.getFullYear() - annoUtente.getFullYear());
+
+            if (oggi.getMonth() < annoUtente.getMonth() ||
+                oggi.getMonth() == annoUtente.getMonth() && oggi.getDate() < annoUtente.getDate()) {
+                eta--;
+            }
+            return eta;
+        };
+    }
+}
+
+btn.addEventListener('click', scrivi)
+
+function scrivi() {
+    let newNome = document.getElementById('nome').value;
+    let newCognome = document.getElementById('cognome').value;
+    let newDataNascita = document.getElementById('dataNascita').value;
+    let tabella = document.getElementById('tabella');
+
+    if (newNome == 0 | newCognome == 0 | newDataNascita == 0) {
+        document.getElementById('errore').innerHTML = 'Compilare i campi...'
+        return
+    }
+
+    else {
+
+        const newUtente = new Utente(newNome, newCognome, newDataNascita);
+        let tr = document.createElement('tr');
+        tr.innerHTML = `<td class="py-3">${newUtente.nome}</td><td class="py-3">${newUtente.cognome}</td><td class="py-3">${newUtente.dataNascita}</td><td class="py-3">${newUtente.anni()}</td>`;
+        tabella.appendChild(tr);
+        let span = document.createElement("span");
+        span.className = "btn btn-sm float-end mt-2";
+        span.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        tr.appendChild(span);
+        span.addEventListener('click', function () {
+            span.parentNode.remove();
+        })
+        svuotaCampi();
+        stampArray();
+    }
+}
+
+
+function svuotaCampi() {
+    document.getElementById('errore').innerHTML = '';
+    document.getElementById('nome').value = '';
+    document.getElementById('cognome').value = '';
+    document.getElementById('dataNascita').value = '';
+}
+
+
