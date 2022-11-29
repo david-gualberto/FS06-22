d0f45e40a671356728edf616fda11faba5955bfe
David Gualberto <gualbertodavid@gmail.com>
2022-11-29 22:27:41 +0100

    modifica es w9 d2

--- m3/w9/d2/src/app/service/posts.service.ts
+++ m3/w9/d2/src/app/service/posts.service.ts
@@ -2,38 +2,20 @@ import { getSafePropertyAccessString } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Posts } from '../modules/posts';
 
-
-
-export function getPosts(){
-    let f = fetch("http://localhost:3000/posts")
-    let r = f.then((res):Promise<Posts[]>=>{
-      if(res.ok){
-        return res.json()
-      }
-      throw new Error("Lettura fallita")
-    })
-    r.catch((err)=>{
-      console.log("Errore", err.getMessage());
-    })
+export function getPosts() {
+    let f = fetch('http://localhost:3000/posts');
+    let r = f.then((res): Promise<Posts[]> => {
+        return res.json();
+    });
     return r;
-  }
+}
 
-  export function getPostFiltered(a:boolean){
-    let t = getPosts().then((res)=>{
-      let arrFiltrato = res.filter((e)=>{return (e.active == a)})
-      return arrFiltrato
-    })
-    return t
-  }
-
-
-
-
-
-
-
-
-
-
-
-
+export function getPostFiltered(a: boolean) {
+    let t = getPosts().then((res) => {
+        let arrFiltrato = res.filter((e) => {
+            return e.active == a;
+        });
+        return arrFiltrato;
+    });
+    return t;
+}
