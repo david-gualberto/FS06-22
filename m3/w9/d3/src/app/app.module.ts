0368ee0f1aa0be26b8561263d5040016d7865a6e
David Gualberto <gualbertodavid@gmail.com>
2022-11-30 13:16:16 +0100

    esercizio w9 d3

---
+++ m3/w9/d3/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { PostAttiviComponent } from './components/post-attivi/post-attivi.component';
+import { PostNonAttiviComponent } from './components/post-non-attivi/post-non-attivi.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ButtonComponent,
+    PostAttiviComponent,
+    PostNonAttiviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
